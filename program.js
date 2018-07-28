aaeb27d68295e783569a9ac0a3c2c4f854d7a3c8
keerthana <keerthanasamyuktha@gmail.com>
2018-07-28 18:05:20 +0100

    JIRA-1234 Node.js sample program's source code for calling rolling a dice object
    This requires a dependency file called dice to run

---
+++ program.js
@@ -0,0 +1,12 @@
+var play = require("./dice");
+var dice = play.game; 
+dice.size = 10;
+console.log(dice.roll());
+console.log(dice.roll());
+console.log(dice.roll());
+console.log("the total number of rolls is "+ dice.totalroll);
+console.log(dice);
+
+
+
+
